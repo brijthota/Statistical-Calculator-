241cd2929839255432e857cf2bd5b4cd4d83e59a
vivek reddy <vivekreddy97@gmail.com>
2019-11-24 18:28:08 -0500

    Fix: unit test fixed

---
+++ Tests/statstest.py
@@ -0,0 +1,23 @@
+import unittest
+
+from Csvreader.Csvreader import csvreader
+from Calculator.Calculator import calculator
+from Statistics.Stats import Statistics
+
+
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.statistics = Statistics('Tests/csvfiles/TestStats.csv')
+
+    # def setUp(self) -> None:
+    # self.calculator = Calculator()
+
+    def test_instantiate_calculator(self):
+        self.assertIsInstance(self.statistics, Statistics)
+
+    def test_decorator_calculator(self):
+        self.assertIsInstance(self.statistics, Statistics)
+
+if __name__ == '__main__':
+    unittest.main()
