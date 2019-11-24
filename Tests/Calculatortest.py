629ae3b63a113540eb6f81b9540cf45315f77326
vivek reddy <vivekreddy97@gmail.com>
2019-11-24 18:21:01 -0500

    Fix: unit test fixed

--- Tests/Calculatortest.py
+++ Tests/Calculatortest.py
@@ -11,46 +11,46 @@ class MyTestCase(unittest.TestCase):
         self.assertIsInstance(self.calculator, calculator)
 
     def test_addition(self):
-        test_data = csvreader('Tests/csvdata/UnitTestAddition.csv').data
+        test_data = csvreader('Tests/csvfiles/TestAddition.csv').data
         for row in test_data:
             result = float(row['Result'])
-            self.assertEqual(self.calculator.addition(row['Value 1'], row['Value 2']), result)
+            self.assertEqual(self.calculator.Addition(row['Value 1'], row['Value 2']), result)
             self.assertEqual(self.calculator.result, result)
 
 
     def test_subtraction(self):
-        test_data = csvreader('Tests/csvdata/UnitTestSubtraction.csv').data
+        test_data = csvreader('Tests/csvfiles/TestSubtraction.csv').data
         for row in test_data:
             result = float(row['Result'])
-            self.assertEqual(self.calculator.subtraction(row['Value 1'], row['Value 2']), result)
+            self.assertEqual(self.calculator.Subtraction(row['Value 1'], row['Value 2']), result)
             self.assertEqual(self.calculator.result,result)
 
     def test_times(self):
-        test_data = csvreader('Tests/csvdata/UnitTestMultiplication.csv').data
+        test_data = csvreader('Tests/csvfiles/TestMultiplication.csv').data
         for row in test_data:
             result = float(row['Result'])
-            self.assertEqual(self.calculator.multiply(row['Value 1'], row['Value 2']), result)
+            self.assertEqual(self.calculator.Multiplication(row['Value 1'], row['Value 2']), result)
             self.assertEqual(self.calculator.result, result)
 
     def test_div(self):
-        test_data = csvreader('Tests/csvdata/UnitTestDivision.csv').data
+        test_data = csvreader('Tests/csvfiles/TestDivision.csv').data
         for row in test_data:
             result = float(row['Result'])
-            self.assertAlmostEqual(self.calculator.division(row['Value 1'], row['Value 2']), result)
+            self.assertAlmostEqual(self.calculator.Division(row['Value 1'], row['Value 2']), result)
             self.assertAlmostEqual(self.calculator.result, result)
 
     def test_square(self):
-        test_data = csvreader('Tests/csvdata/UnitTestSquare.csv').data
+        test_data = csvreader('Tests/csvfiles/TestSquare.csv').data
         for row in test_data:
             result = float(row['Result'])
-            self.assertEqual(self.calculator.square_(row['Value 1']), result)
+            self.assertEqual(self.calculator.Square(row['Value 1']), result)
             self.assertEqual(self.calculator.result, result)
 
     def test_sqrt(self):
-        test_data = csvreader('Tests/csvdata/UnitTestSquareRoot.csv').data
+        test_data = csvreader('Tests/csvfiles/TestSquareRoot.csv').data
         for row in test_data:
             result = float(row['Result'])
-            self.assertAlmostEqual(self.calculator.sqrt_(row['Value 1']), result)
+            self.assertAlmostEqual(self.calculator.Sqrt(row['Value 1']), result)
             self.assertAlmostEqual(self.calculator.result, result)
 
     def test_results_property(self):
