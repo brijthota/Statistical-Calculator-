1ce5fed824320a704ce103c78f9f482776686df3
vivek reddy <vivekreddy97@gmail.com>
2019-12-07 23:14:12 -0500

    Fix: Sqlite update

--- Tests/statstest.py
+++ Tests/statstest.py
@@ -1,23 +1,85 @@
 import unittest
 
-from Csvreader.Csvreader import csvreader
-from Calculator.Calculator import Calculator
-from Statistics.Stats import Statistics
+from Csvreader.csvreader import csvreader
+from Statistics.statistics import statistics
 
 
 
 class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
-        self.statistics = Statistics('Tests/csvfiles/TestStats.csv')
-
-    # def setUp(self) -> None:
-    # self.calculator = Calculator()
+        self.statisticss = statistics()
 
     def test_instantiate_calculator(self):
-        self.assertIsInstance(self.statistics, Statistics)
+        self.assertIsInstance(self.statisticss, statistics)
 
-    def test_decorator_calculator(self):
-        self.assertIsInstance(self.statistics, Statistics)
+    def test_median(self):
+        test_data = csvreader('Tests/csvdata/Array3.csv').data
+        test_result = csvreader('Tests/csvdata/Array3_result2.csv').data
+
+        for column in test_result:
+            result_test = float(column['Medain'])
+
+        listx = []
+
+        for row in test_data:
+            result = float(row['Array'])
+            listx.append(result)
+
+        self.statisticss.median_(listx)
+        self.assertEqual(round(self.statisticss.result), round(result_test))
+
+    def test_mode(self):
+        test_data = csvreader('Tests/csvdata/Array3.csv').data
+        test_result = csvreader('Tests/csvdata/Array3_result2.csv').data
+
+        for column in test_result:
+            result_test = float(column['Mode'])
+
+        listx = []
+
+        for row in test_data:
+            result = float(row['Array'])
+            listx.append(result)
+
+        self.statisticss.mode_(listx)
+        self.assertEqual(round(self.statisticss.result), round(result_test))
+
+    def test_mean(self):
+        test_data = csvreader('Tests/csvdata/Array3.csv').data
+        test_result = csvreader('Tests/csvdata/Array3_result2.csv').data
+
+        for column in test_result:
+            result_test = float(column['Mean'])
+
+
+        listx = []
+
+        for row in test_data:
+            result = float(row['Array'])
+            listx.append(result)
+
+
+        self.statisticss.mean_(listx)
+        self.assertEqual(round(self.statisticss.result), round(result_test))
+
+    def test_stdev(self):
+        test_data = csvreader('Tests/csvdata/Array3.csv').data
+        test_result = csvreader('Tests/csvdata/Array3_result2.csv').data
+
+        for column in test_result:
+            result_test = float(column['Stdev'])
+
+        listx = []
+
+        for row in test_data:
+            result = float(row['Array'])
+            listx.append(result)
+
+        self.statisticss.stdev_(listx)
+        self.assertEqual(round(self.statisticss.result), round(result_test))
+
+    def test_results_property(self):
+        self.assertEqual(self.statisticss.result, 0)
 
 if __name__ == '__main__':
     unittest.main()
